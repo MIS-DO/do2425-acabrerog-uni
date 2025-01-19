2c628b2e0776750bcc16ca0750e9efc9f0fa56e4
Antonio Cabrero Garcia <antcabgar1@alum.us.es>
2025-01-19 22:14:33 +0100

    Realizado servicio findApartmentsByRef

--- services/apiv1apartmentsapartmentRefService.js
+++ services/apiv1apartmentsapartmentRefService.js
@@ -1,7 +1,32 @@
+'use strict'
+
+var db = require('../db');
+
+
 module.exports.findByapartmentRef = function findByapartmentRef(req, res) {
-    res.send({
-        message: 'This is the mockup controller for findByapartmentRef'
-    });
+    var apartmentRef = req.params.apartmentRef;
+    if (!apartmentRef) {
+        console.warn("New GET request to /apartment/:apartmentRef without apartmentRef, sending 400...");
+        res.status(400).send(); // bad request
+    } else {
+        console.info("New GET request to /apartments/" + apartmentRef);
+        db.find({ "apartmentRef": parseInt(apartmentRef) }, function (err, filteredApartments) {
+            if (err) {
+                console.error('Error getting data from DB');
+                res.status(500).send(); // internal server error
+            } else {
+                if (filteredApartments.length > 0) {
+                    var apartment = filteredApartments[0]; //since we expect to have exactly ONE apartment with this name
+                    console.info("Sending apartment: " + JSON.stringify(apartment, 2, null));
+                    res.send(apartment);
+                } else {
+                    console.warn("There are no apartments with apartmentRef " + apartmentRef);
+                    res.status(404).send(); // not found
+                }
+            }
+        });
+    }
+
 }
 
 module.exports.updateApartment = function updateApartment(req, res) {
