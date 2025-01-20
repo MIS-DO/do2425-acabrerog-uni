180a555fae337b8e5a368e68a780c35c832d86e1
Antonio Cabrero Garcia <antcabgar1@alum.us.es>
2025-01-20 23:22:43 +0100

    Implentación de function updateApartment

--- services/apiv1apartmentsapartmentRefService.js
+++ services/apiv1apartmentsapartmentRefService.js
@@ -56,57 +56,40 @@ module.exports.deleteApartment = function deleteApartment(req, res) {
 }
 
 module.exports.updateApartment = function updateApartment(req, res) {
+    
+    var updatedApartment = req.body;
+    console.info("req.body: " + JSON.stringify(req.body, 2, null));
+    
     var apartmentRef = req.params.apartmentRef;
-    if (!apartmentRef) {
-        console.warn("New DELETE request to /apartment/:apartmentRef without apartmentRef, sending 400...");
-        res.status(400).send(); // bad request
-    } else {
-        console.info("New DELETE request to /apartments/" + apartmentRef);
-        db.remove({ "apartmentRef": parseInt(apartmentRef) }, function (err, ApartmentRemoved) {
-            if (err) {
-                console.error('Error removing data from DB');
-                res.status(500).send(); // internal server error
-            } else {
-                console.info("Apartment removed: " + ApartmentRemoved);
-                if (ApartmentRemoved ==  1) {
-                    console.info("The apartment with ref: " + apartmentRef+ " has been succesfully deleted, sending 204...");
-                    res.status(204).send(); // no content
-                } else {
-                    console.warn("There are no apartments to delete ");
-                    res.status(404).send(); // not found
-                }
-            }
-        });
-    }
-}
+    console.info("apartmentRef: " + apartmentRef);
 
-/*
-    var updatedContact = req.body;
-    var name = req.params.name;
-    if (!updatedContact) {
-        console.warn("New PUT request to /contacts/ without contact, sending 400...");
+    if (!updatedApartment) {
+        console.warn("New PUT request to /apartment/:apartmentRef without apartmentRef, sending 400...");
         res.status(400).send(); // bad request
     } else {
-        console.info("New PUT request to /contacts/" + name + " with data " + JSON.stringify(updatedContact, 2, null));
-        if (!updatedContact.name || !updatedContact.phone || !updatedContact.email) {
-            console.warn("The contact " + JSON.stringify(updatedContact, 2, null) + " is not well-formed, sending 422...");
+        console.info("New PUT request to /apartments/" + apartmentRef + " with data " + JSON.stringify(updatedApartment, 2, null));
+        //if (!updatedApartment.apartmentRef || !updatedApartment.country || !updatedApartment.city || !updatedApartment.area || !updatedApartment.type || !updatedApartment.m2 || !updatedApartment.hasGarage) {
+        if (updatedApartment.apartmentRef == null || !updatedApartment.country || !updatedApartment.city || !updatedApartment.area || !updatedApartment.type || updatedApartment.m2 == null || updatedApartment.hasGarage === null) {
+            console.warn("The apartment " + JSON.stringify(updatedApartment, 2, null) + " is not well-formed, sending 422...");
             res.status(422).send(); // unprocessable entity
         } else {
-            db.find({ "name": updatedContact.name }, function (err, contacts) {
+            console.info("searching for apartment with apartmentRef: " + apartmentRef);
+            db.find({ "apartmentRef": parseInt(apartmentRef) }, function (err, apartments) {
                 if (err) {
                     console.error('Error getting data from DB');
                     res.status(500).send(); // internal server error
                 } else {
-                    if (contacts.length > 0) {
-                        db.update({ name: name }, updatedContact);
-                        console.info("Modifying contact with name " + name + " with data " + JSON.stringify(updatedContact, 2, null));
+                    if (apartments.length > 0) {
+                        db.update({ "apartmentRef": parseInt(apartmentRef) }, updatedApartment);
+                        console.info("Modifying apartment with apartmentRef " + apartmentRef + " with data " + JSON.stringify(updatedApartment, 2, null));
                         res.status(204).send(); // no content
+                        console.info("Apartment updated: " + JSON.stringify(updatedApartment, 2, null));
                     } else {
-                        console.warn("There are not any contact with name " + name);
+                        console.warn("There are not any apartment with apartmentRef " + apartmentRef);
                         res.status(404).send(); // not found
                     }
                 }
             });
         }
     }
-*/
+}
